6466ab889a18fd2bb7154d9ef4fef5e73be0ecb8
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-03-31 16:44:31 +0100

    Try to set libclang for ubuntu

--- test.macos.sh
+++ test.macos.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
-
-
+# Virtual environment setup
 python3 -m venv .venv           # Create a Python virtual env
 source ./.venv/bin/activate     # Activate the virtual env for bash by source.
 
+# Type checks
 python -m mypy *.py --check-untyped-defs  # Run mypy to check type hints
+
+Unit tests
 PY_CPPMODEL_LIBCLANG_PATH=/Library/Developer/CommandLineTools/usr/lib/libclang.dylib \
-python -m unittest discover .             # Run tests
+python -m unittest discover --verbose .             # Run tests
