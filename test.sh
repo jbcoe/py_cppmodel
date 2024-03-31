5d109d6d0db2eedbacc06ecb7bf464d66b65f8bc
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-03-31 15:57:08 +0100

    Try to fix python workflow

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ set -x
 python3 -m venv .venv           # Create a Python virtual env
 source ./.venv/bin/activate     # Activate the virtual env for bash by source.
 
-mypy *.py --check-untyped-defs  # Run mypy to check type hints
-unittest discover .             # Run tests
+python -m mypy *.py --check-untyped-defs  # Run mypy to check type hints
+python -m unittest discover .             # Run tests
