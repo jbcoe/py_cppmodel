8f3cfa9cd61002b565b8c6d7ea54a3a06cf69d19
Jonathan B. Coe <jonathanbcoe@gmail.com>
2026-01-31 10:57:45 +0000

    Modernize project to use pyproject.toml

--- test.macos.sh
+++ test.macos.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Virtual environment setup
-python3 -m venv .venv           # Create a Python virtual env
-source ./.venv/bin/activate     # Activate the virtual env for bash by source.
-python3 -m pip install -r requirements.txt # Install latest requirements.
+# Install dependencies
+uv sync
 
 # Type checks
-python3 -m mypy *.py --check-untyped-defs  # Run mypy to check type hints
+uv run mypy *.py --check-untyped-defs  # Run mypy to check type hints
 
 # Unit tests
 PY_CPPMODEL_LIBCLANG_PATH=/Library/Developer/CommandLineTools/usr/lib/libclang.dylib \
-python3 -m unittest discover --verbose .             # Run tests
+uv run python3 -m unittest discover --verbose .             # Run tests
