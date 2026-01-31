dacf1ed2262d694a412cf29ad366975fda3227b9
Jonathan B. Coe <jonathanbcoe@gmail.com>
2026-01-31 18:37:29 +0000

    Simplify tests by including C++ source inline (#15)

--- test_py_cppmodel.py
+++ test_py_cppmodel.py
@@ -12,12 +12,35 @@ COMPILER_ARGS = [
     "-I/Library/Developer/CommandLineTools/usr/include",
 ]
 
+SOURCE = """\
+int z = 0;
+
+struct __attribute__((annotate("A"))) A {
+  int a;
+  double b;
+  char c[8];
+
+  __attribute__((annotate("foo"))) int foo(int);
+};
+
+template <class T>
+class B {
+  T t;
+  T wibble(T);
+};
+
+double bar(double);
+
+int main() {}
+"""
+
 
 class TestCppModel(unittest.TestCase):
     def setUp(self):
         tu = TranslationUnit.from_source(
             "sample.cc",
             COMPILER_ARGS,
+            unsaved_files=[("sample.cc", SOURCE)],
         )
         self.model = py_cppmodel.Model(tu)
 
